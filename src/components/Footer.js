7ce5a719e2acaa09ed9c0b6be6c13049069a3a33
Shreeya Shrestha <shreeya533s@gmail.com>
2022-06-03 20:44:57 +0545

    design file changed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div
       style={{
-        paddingTop: "1rem",
+        padding: "5rem",
         backgroundColor: "#0e214d",
         color: "white",
       }}
@@ -37,12 +37,15 @@ const Footer = () => {
             flexDirection: "column",
           }}
         >
-          <h3 style={{ color: "white" }}>Popular Resources</h3>
-          <p>All Journal</p>
-          <p>Asset Allocation</p>
-          <p>Investing Basics</p>
-          <p>Model Portfolios</p>
-          <p>Stock Ideas</p>
+          <h3 style={{ color: "white" }}>More on AAII.com</h3>
+          <p>AAII Store</p>
+          <p>AAII Guides</p>
+          <p>AAII Investor Classroom</p>
+          <p>Local Chapters</p>
+          <p>Financial Planning</p>
+          <p>Investor Survey</p>
+          <p>AAII publishing calendar</p>
+
         </div>
         <div
           style={{
@@ -51,12 +54,14 @@ const Footer = () => {
             flexDirection: "column",
           }}
         >
-          <h3 style={{ color: "white" }}>Popular Resources</h3>
-          <p>All Journal</p>
-          <p>Asset Allocation</p>
-          <p>Investing Basics</p>
-          <p>Model Portfolios</p>
-          <p>Stock Ideas</p>
+          <h3 style={{ color: "white" }}>Popular AAII Products</h3>
+          <p>A+ Investors</p>
+          <p>VMQ Stocks</p>
+          <p>Level3</p>
+          <p>AAII Dividend Investing</p>
+          <p>Stock Investor Pro</p>
+          <p>Stock Superstars Report</p>
+
         </div>
         <div
           style={{
@@ -65,12 +70,12 @@ const Footer = () => {
             flexDirection: "column",
           }}
         >
-          <h3 style={{ color: "white" }}>Popular Resources</h3>
-          <p>All Journal</p>
-          <p>Asset Allocation</p>
-          <p>Investing Basics</p>
-          <p>Model Portfolios</p>
-          <p>Stock Ideas</p>
+          <h3 style={{ color: "white" }}>Popular AAII Chapters</h3>
+          <p>AAII Baltimore Chapter </p>
+          <p>AAII Denver Chapter</p>
+          <p>AAII Los Angeles Chapter</p>
+          <p>AAII Pittsburgh Chapter</p>
+          <p>AAII Washington D.C Metro Chapter</p>
         </div>
       </div>
       <div
@@ -80,29 +85,29 @@ const Footer = () => {
           The American Association of Individual Investor is an idependent,
           noprofit corporation formed for the purpose of assisting individuals
           in becoming effevtive managers of their own assets through programs of
-          education,information and research
+          education,information and research. Learn More
         </p>
         <p>©2022 Americal Association of Inidvidual Investors</p>
         <div>
-          <a style={{ color: "white" }} href="/">
+          <a className="footer_test" href="/">
             TOP
           </a>
-          <a style={{ color: "white", marginRight: "10px" }} href="/">
+          <a className="footer_test" href="/">
             HOME
           </a>
-          <a style={{ color: "white", marginRight: "10px" }} href="/">
+          <a className="footer_test" href="/">
             CONTACT US
           </a>
-          <a style={{ color: "white", marginRight: "10px" }} href="/">
+          <a className="footer_test" href="/">
             WEBSITE HELP
           </a>
-          <a style={{ color: "white", marginRight: "10px" }} href="/">
+          <a className="footer_test" href="/">
             SALES TAX FAQS
           </a>
-          <a style={{ color: "white", marginRight: "10px" }} href="/">
+          <a className="footer_test" href="/">
             PRIVACY POLICY
           </a>
-          <a style={{ color: "white", marginRight: "10px" }} href="/">
+          <a className="footer_test" href="/">
             DISCLAIMER
           </a>
         </div>
